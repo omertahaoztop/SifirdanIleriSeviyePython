6c9c7c6c80982d8f3d65c3e57a9345f0c7607808
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2020-07-14 18:46:55 +0300

    Sıfırdan İleri Seviye Python

---
+++ Python Objeleri ve Veri Tipleri/tuple.py
@@ -0,0 +1,11 @@
+liste=[1,2,3] #liste tanımlaması
+tuple=(1,'iki',3) #tuple tanımlaması
+
+print(type(liste))  #list
+print(type(tuple)) #tuple
+
+liste=['ömer','enes']
+tuple=('ömer','enes','semih')
+
+print(tuple.count('ömer'))
+print(tuple.index('ömer'))
